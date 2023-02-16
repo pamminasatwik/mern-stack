da12ac77b3c906996810948daadae3912a14be85
Pammina Satwik <92246119+pamminasatwik@users.noreply.github.com>
2023-02-16 10:51:23 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,48 @@
+// require('dotenv').config();
+const userLib = require("./backend/lib/userLib");
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/resume', function(req, res) {
+    res.sendFile(__dirname + '/resume.html');
+});
+
+app.get('/card', function(req, res) {
+    res.sendFile(__dirname + '/card.html');
+});
+
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGO_CONNECTION_STRING, {}, function(err) {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log('Connected to database');
+        // TODO : donot create a user if atleast 1 user exist in the table
+        userLib.getAllUsers(function(err, usersList) {
+            if (err) {
+                console.error(err);
+            } else {
+                if (usersList.length === 0) {
+                    userLib.createFirstUser(function(err, res) {
+                        if (err) {
+                            console.error(err);
+                        } else {
+                            console.log(res);
+                        }
+                    });
+                }
+            }
+        });
+        app.listen(port, function() {
+            console.log('Server started on port ' + port);
+        });
+    }
+});
