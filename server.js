ea46268fc1f45db6b6a1a6edb81afad843c53991
Satwik <20h51a0543@cmrcet.ac.in>
2023-02-16 11:13:29 +0530

    hello

--- server.js
+++ server.js
@@ -1,48 +1,61 @@
-// require('dotenv').config();
-const userLib = require("./backend/lib/userLib");
+// // require('dotenv').config();
+// const userLib = require("./backend/lib/userLib");
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const app = express();
+// const port = process.env.PORT || 3000;
+
+// app.use(express.static(__dirname + '/public'));
+
+// app.get('/', function(req, res) {
+//     res.sendFile(__dirname + '/index.html');
+// });
+
+// app.get('/resume', function(req, res) {
+//     res.sendFile(__dirname + '/resume.html');
+// });
+
+// app.get('/card', function(req, res) {
+//     res.sendFile(__dirname + '/card.html');
+// });
+
+// mongoose.set('strictQuery', true);
+// mongoose.connect(process.env.MONGO_CONNECTION_STRING, {}, function(err) {
+//     if (err) {
+//         console.error(err);
+//     } else {
+//         console.log('Connected to database');
+//         // TODO : donot create a user if atleast 1 user exist in the table
+//         userLib.getAllUsers(function(err, usersList) {
+//             if (err) {
+//                 console.error(err);
+//             } else {
+//                 if (usersList.length === 0) {
+//                     userLib.createFirstUser(function(err, res) {
+//                         if (err) {
+//                             console.error(err);
+//                         } else {
+//                             console.log(res);
+//                         }
+//                     });
+//                 }
+//             }
+//         });
+//         app.listen(port, function() {
+//             console.log('Server started on port ' + port);
+//         });
+//     }
+// });
+
 const express = require('express');
-const mongoose = require('mongoose');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5010;
 
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+app.get("/", function(req, res){
+	res.send("I am satwik");
 });
 
-app.get('/resume', function(req, res) {
-    res.sendFile(__dirname + '/resume.html');
-});
-
-app.get('/card', function(req, res) {
-    res.sendFile(__dirname + '/card.html');
-});
-
-mongoose.set('strictQuery', true);
-mongoose.connect(process.env.MONGO_CONNECTION_STRING, {}, function(err) {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log('Connected to database');
-        // TODO : donot create a user if atleast 1 user exist in the table
-        userLib.getAllUsers(function(err, usersList) {
-            if (err) {
-                console.error(err);
-            } else {
-                if (usersList.length === 0) {
-                    userLib.createFirstUser(function(err, res) {
-                        if (err) {
-                            console.error(err);
-                        } else {
-                            console.log(res);
-                        }
-                    });
-                }
-            }
-        });
-        app.listen(port, function() {
-            console.log('Server started on port ' + port);
-        });
-    }
+app.listen(port, function(){
+	console.log("Server running on http://localhost:"+port);
+	console.log(`Server running on http://localhost:${port}`);
 });
