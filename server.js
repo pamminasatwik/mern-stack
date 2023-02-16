3b6426be8f30594da6ad894a0aaee730843cee28
Satwik <20h51a0543@cmrcet.ac.in>
2023-02-16 12:08:20 +0530

    gone

--- server.js
+++ server.js
@@ -50,6 +50,11 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5010;
+const options = {
+    extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
+    index: ['index.html'],  
+}
+app.use(express.static("public",options));
 
 app.get("/", function(req, res){
 	// res.send("I am satwik");
@@ -58,6 +63,10 @@ app.get("/", function(req, res){
 
 app.get('/resume', function(req, res) {
     res.sendFile(__dirname + '/resume.html');
+});
+
+app.get('/card', function(req, res) {
+    res.sendFile(__dirname + '/card.html');
 });
 
 app.listen(port, function(){
